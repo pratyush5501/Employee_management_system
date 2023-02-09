890bdc7c45f996f0edbf43867d71483b18ff2c6a
pratyush5501 <71936387+pratyush5501@users.noreply.github.com>
2023-02-09 11:56:14 +0530

    Add files via upload
    Innitial commit while setting up the project. With controller ,  model , repository and exception folders. DB yet to be configured.

---
+++ springboot-backend/sprinngboot-backend/src/main/java/net/javaguides/sprinngbootbackend/SprinngbootBackendApplication.java
@@ -0,0 +1,13 @@
+package net.javaguides.sprinngbootbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinngbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinngbootBackendApplication.class, args);
+	}
+
+}
---
+++ springboot-backend/sprinngboot-backend/src/test/java/net/javaguides/sprinngbootbackend/SprinngbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javaguides.sprinngbootbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinngbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
